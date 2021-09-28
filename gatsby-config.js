9ce979bf536c6924d530e8dc908c3defa9e55d90
Brandon <brandongines@gmail.com>
2021-09-28 09:53:44 -0400

    icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/atw.png`,
+        icon: `src/images/mcghee_face_seal.png`,
       },
     },
     `gatsby-transformer-yaml`,
