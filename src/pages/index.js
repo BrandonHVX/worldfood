ca2e1666174260f1bb924046ab11a3fb7194a148
Brandon <brandongines@gmail.com>
2021-09-29 14:40:49 -0400

    form update

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Team from "../subpages/Team"
 import End from "../subpages/End"
 import Footer from "../components/Footer"
 import React, { Component } from "react"
-import Pic from "../images/form-v8.jpg"
+import Pic from "../images/tailgate.png"
 
 const encode = data => {
   return Object.keys(data)
@@ -58,17 +58,7 @@ export default class index extends Component {
             <img src={Pic} alt="form" />
           </div>
           <div class="form-right">
-            <div class="tab">
-              <div class="tab-inner">
-                <button
-                  class="tablinks"
-                  onclick="openCity(event, 'sign-up')"
-                  id="defaultOpen"
-                >
-                  Sign Up
-                </button>
-              </div>
-            </div>
+            <div class="tab"></div>
             <form
               class="form-detail"
               method="POST"
@@ -82,140 +72,150 @@ export default class index extends Component {
               data-netlify-honeypot="bot-field"
               onSubmit={this.handleSubmit}
             >
-              <div class="col-md-6">
-                <label for="inputEmail4" class="form-label">
-                  Email
-                </label>
-                <input
-                  class="form-input"
-                  id="validationTooltipUsername"
-                  name="businessname"
-                  value={businessname}
-                  onChange={this.handleChange}
-                  type="text"
-                  placeholder="Name Of Business"
-                  aria-describedby="validationTooltipUsernamePrepend"
-                  required
-                />
-              </div>
-              <div class="col-md-6">
-                <label for="inputEmail4" class="form-label">
-                  Email
-                </label>
-                <input
-                  class="form-input"
-                  name="businessowner"
-                  value={businessowner}
-                  onChange={this.handleChange}
-                  type="text"
-                  placeholder="Business Owner's Name"
-                  required
-                  id="validationTooltipUsername"
-                />
-              </div>
-              <div class="col-12">
-                <label for="inputEmail4" class="form-label">
-                  Email
-                </label>
-                <input
-                  name="businessphone"
-                  value={businesphone}
-                  onChange={this.handleChange}
-                  type="number"
-                  class="form-input"
-                  placeholder="Telephone"
-                  required
-                />
-              </div>
-              <div class="col-12">
-                <label for="inputEmail4" class="form-label">
-                  Email
-                </label>
-                <input
-                  name="businessemail"
-                  value={businessemail}
-                  onChange={this.handleChange}
-                  type="email"
-                  class="form-input"
-                  placeholder="Email"
-                  required
-                />
-              </div>
-              <div class="col-md-12">
-                <label for="inputEmail4" class="form-label">
-                  Email
-                </label>
-                <input
-                  class="form-input"
-                  type="name"
-                  name="businessaddress"
-                  value={businessaddress}
-                  onChange={this.handleChange}
-                  placeholder="Address"
-                  required
-                />
-              </div>
-              <div class="col-md-4">
-                <label for="inputEmail4" class="form-label">
-                  Email
-                </label>
-                <select
-                  name="businessstate"
-                  value={businessstate}
-                  onChange={this.handleChange}
-                  type="text"
-                  class="form-input"
-                  placeholder=" State"
-                  requiredid="inputState"
+              <div className="row ">
+                <div
+                  class="col-md-12 
+              "
                 >
-                  <option selected>State...</option>
-                  <option>Florida</option>
-                </select>
-              </div>
-              <div class="col-md-4">
-                <label for="inputEmail4" class="form-label">
-                  Email
-                </label>
-                <input
-                  class="form-input"
-                  name="businesscity"
-                  value={businesscity}
-                  onChange={this.handleChange}
-                  type="text"
-                  placeholder=" City"
-                  required
-                />
-              </div>
-              <div class="col-md-4">
-                <label for="inputEmail4" class="form-label">
-                  Email
-                </label>
-                <input
-                  class="form-input"
-                  name="businesszip"
-                  value={businesszip}
-                  onChange={this.handleChange}
-                  type="text"
-                  placeholder="Zip"
-                  required
-                />
-              </div>
-              <div class="col-12">
-                <div class="form-check">
-                  <input
-                    class="form-check-input"
-                    type="checkbox"
-                    id="gridCheck"
-                  />
-                  <label class="form-check-label" for="gridCheck">
-                    Check me out
+                  <label for="inputEmail4" class="form-label">
+                    Business Name
                   </label>
+                  <input
+                    class="form-input"
+                    id="validationTooltipUsername"
+                    name="businessname"
+                    value={businessname}
+                    onChange={this.handleChange}
+                    type="text"
+                    placeholder="Name Of Business"
+                    aria-describedby="validationTooltipUsernamePrepend"
+                    required
+                  />
                 </div>
-              </div>
-              <div class="col-12">
-                <button type="submit" class="btn btn-primary">
-                  Sign in
-                </button>
+                <div
+                  class="col-md-12
+              "
+                >
+                  <label for="inputEmail4" class="form-label">
+                    Business Owner Name
+                  </label>
+                  <input
+                    class="form-input"
+                    name="businessowner"
+                    value={businessowner}
+                    onChange={this.handleChange}
+                    type="text"
+                    placeholder="Business Owner's Name"
+                    required
+                    id="validationTooltipUsername"
+                  />
+                </div>
+
+                <div class="col-md-12">
+                  <label for="inputEmail4" class="form-label">
+                    Phone
+                  </label>
+                  <input
+                    name="businessphone"
+                    value={businesphone}
+                    onChange={this.handleChange}
+                    type="number"
+                    class="form-input"
+                    placeholder="Telephone"
+                    required
+                  />
+                </div>
+                <div class="col-md-12">
+                  <label for="inputEmail4" class="form-label">
+                    Email
+                  </label>
+                  <input
+                    name="businessemail"
+                    value={businessemail}
+                    onChange={this.handleChange}
+                    type="email"
+                    class="form-input"
+                    placeholder="Email"
+                    required
+                  />
+                </div>
+                <div class="col-md-12">
+                  <label for="inputEmail4" class="form-label">
+                    Address
+                  </label>
+                  <input
+                    class="form-input"
+                    type="name"
+                    name="businessaddress"
+                    value={businessaddress}
+                    onChange={this.handleChange}
+                    placeholder="Address"
+                    required
+                  />
+                </div>
+                <div class="col-md-12">
+                  <label for="inputEmail4" class="form-label">
+                    City
+                  </label>
+                  <input
+                    class="form-input"
+                    name="businesscity"
+                    value={businesscity}
+                    onChange={this.handleChange}
+                    type="text"
+                    placeholder=" City"
+                    required
+                  />
+                </div>
+                <div class="col-md-12">
+                  <label for="inputEmail4" class="form-label">
+                    State
+                  </label>
+                  <select
+                    name="businessstate"
+                    value={businessstate}
+                    onChange={this.handleChange}
+                    type="text"
+                    class="form-input"
+                    placeholder=" State"
+                    requiredid="inputState"
+                  >
+                    <option selected>State...</option>
+                    <option>Florida</option>
+                  </select>
+                </div>
+
+                <div class="col-md-12">
+                  <label for="inputEmail4" class="form-label">
+                    Email
+                  </label>
+                  <input
+                    class="form-input"
+                    name="businesszip"
+                    value={businesszip}
+                    onChange={this.handleChange}
+                    type="text"
+                    placeholder="Zip"
+                    required
+                  />
+                </div>
+                <div class="col-12">
+                  <div class="form-check">
+                    <input
+                      class="form-check-input"
+                      type="checkbox"
+                      id="gridCheck"
+                    />
+                    <label class="form-check-label" for="gridCheck">
+                      Click the Submit Button to Register your Business
+                    </label>
+                  </div>
+                </div>
+                <div class="col-12">
+                  <button type="submit" class="btn btn-primary">
+                    Submit
+                  </button>
+                </div>
               </div>
             </form>
           </div>
