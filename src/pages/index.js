fec761a333f7419788edceea0fce7414b0aff378
Brandon <brandongines@gmail.com>
2021-10-18 11:26:46 -0400

    new image

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Team from "../subpages/Team"
 import End from "../subpages/End"
 import Footer from "../components/Footer"
 import React, { Component } from "react"
-import Pic from "../images/new.png"
+import Pic from "../images/UMvFSU.jpg"
 
 const encode = data => {
   return Object.keys(data)
